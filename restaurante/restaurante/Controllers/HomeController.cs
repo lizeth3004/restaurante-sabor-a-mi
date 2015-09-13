9eba3969c845e9137dc722cbfd09bff6e2923b91
lizeth3004 <lizethvalencia97@gmail.com>
2015-09-12 20:25:46 -0500

    lista de tipo doc y mesas

--- restaurante/restaurante/Controllers/HomeController.cs
+++ restaurante/restaurante/Controllers/HomeController.cs
@@ -3,15 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using restaurante.Models;
 
 namespace restaurante.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
+        restauranteEntities entidad = new restauranteEntities();
         public ActionResult Index()
         {
+            var varTipoDoc = (from td in entidad.tblTipoDoc
+                              select td).ToList();
+            var listTipDoc = new SelectList(varTipoDoc, "idTipoDoc", "nombreTipoDoc");
+            ViewData["TipoDocumento"] = listTipDoc;
+
+            var varMesa = (from td in entidad.tblMesa
+                              select td).ToList();
+            var listMesa = new SelectList(varMesa, "idMesa", "nroMesa");
+            ViewData["Mesa"] = listMesa;
+
+            //var varPlato = (from td in entidad.tblPlato
+            //               select td).ToList();
+            //var listPlato = new SelectList(varPlato, "idPlato", "nombrePlato");
+            //ViewData["Plato"] = listPlato;
+
             return View();
+            
         }
+
+        public ActionResult listarPlato()
+        {
+            var varPlato = (from td in entidad.tblPlato
+                           select td);
+            //var listPlato = new SelectList(varPlato, "idPlato", "nombrePlato");
+            //ViewData["Plato"] = listPlato;
+
+            return View(varPlato.ToList());
+        }
+        
     }
 }
